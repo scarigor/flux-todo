adcbbde7103f81d131be54380f6b9a031bd4aef6
scarigor <kurchenkoigor1@gmail.com>
2017-12-24 04:52:40 +0200

    done.

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export default {
   todos: {
     addTodo: (text) => axios.post("/todos/addtodo", { text }).then(res => res.data),
-    removeTodo: (id) => axios.delete('/todos/' + id).then(res => res.data._id),
-    toggleTodo: (id) => axios.patch('/todos/' + id).then(res => res.data._id),
+    removeTodo: (id) => axios.delete(`/todos/todo-${id}`).then(res => res.data._id),
+    toggleTodo: (id) => axios.patch(`/todos/todo-${id}`).then(res => res.data._id),
     fetchTodos: () => axios.get('/todos').then(res => res.data)
   }
 }
